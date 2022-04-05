bd85fa3f1cfc0b893a107d0e04d6943692aa1cd0
aleksandarkozelj@gmail.com <aleksandarkozelj@gmail.com>
2022-04-05 09:30:41 +0200

    comp move calc working

--- src/components/Content/GameWindow.js
+++ src/components/Content/GameWindow.js
@@ -21,7 +21,7 @@ const GameWindow = () => {
    const delay = (t) => {
       return new Promise((resolve) => { setTimeout(() => resolve(), t) })
    }
-   
+
 
    const calculus = (playerCard, cardsInCalc) => {
       let acc = 0;
@@ -81,94 +81,96 @@ const GameWindow = () => {
 
    }
 
+   const combinations = []
+
+   const checkComputerCalculus = (arr, aceValue = false) => {
+      console.log("🚀 ~ file: GameWindow.js ~ line 87 ~ checkComputerCalculus ~ aceValue", aceValue)
+      let cardValue
+
+      arr.map((card, i) => {
+         let combi = []
+         cardValue = aceValue ? 11 : card.calculusValue
+         console.log("🚀 ~ file: GameWindow.js ~ line 93 ~ arr.map ~ cardValue", cardValue)
+         // same card + a + b + c + d ...
+         store.cardsOnTable.map((tableCardA, a) => {
+            if (aceValue && cardValue !== 11) return
+            if (tableCardA.calculusValue > cardValue) return
+            if (tableCardA.calculusValue === cardValue) {
+               if (combi && combi.includes(card)) { combi.push(tableCardA) } else { combi.push(card, tableCardA) }
+            }
+
+            store.cardsOnTable.map((tableCardB, b) => {
+               if (b === a) return
+               if (tableCardA === tableCardB) return
+               if (tableCardB.calculusValue >= cardValue) return
+               if (tableCardA.calculusValue + tableCardB.calculusValue > cardValue) return
+               if (tableCardA.calculusValue + tableCardB.calculusValue === cardValue) {
+                  if (combi && combi.includes(tableCardA) || combi.includes(tableCardB)) return
+                  if (combi && combi.includes(card)) { combi.push(tableCardA, tableCardB) } else { combi.push(card, tableCardA, tableCardB) }
+               }
+               let isAceThere = false;
+               if (tableCardA.calculusValue === 1 || tableCardB.calculusValue === 1) { isAceThere = true }
+               if (tableCardA.calculusValue + tableCardB.calculusValue < cardValue && isAceThere) {
+                  if (tableCardA.calculusValue + tableCardB.calculusValue + 10 === cardValue) {
+                     console.log('IT CHECKS FOR ACE !@#@!#@!!@!#$!$@!#@!$@$#$#@$#@@@###')
+                     if (combi && combi.includes(tableCardA) || combi.includes(tableCardB)) return
+                     if (combi && combi.includes(card)) { combi.push(tableCardA, tableCardB) } else { combi.push(card, tableCardA, tableCardB) }
+                  }
+               }
+
+               store.cardsOnTable.map((tableCardC, c) => {
+                  if (c === a || c === b) return
+                  if (tableCardA === tableCardB || tableCardA === tableCardC || tableCardB === tableCardC) return
+                  if (tableCardC.calculusValue >= cardValue) return
+                  if (tableCardA.calculusValue + tableCardB.calculusValue + tableCardC.calculusValue > cardValue) return
+                  if (tableCardA.calculusValue + tableCardB.calculusValue + tableCardC.calculusValue === cardValue) {
+                     if (combi && combi.includes(tableCardA) || combi.includes(tableCardB) || combi.includes(tableCardC)) return
+                     if (combi && combi.includes(card)) { combi.push(tableCardA, tableCardB, tableCardC) } else { combi.push(card, tableCardA, tableCardB, tableCardC) }
+                  }
+
+                  store.cardsOnTable.map((tableCardD, d) => {
+                     if (d === a || d === b || d === c) return
+                     if (tableCardD === tableCardA || tableCardD === tableCardB || tableCardD === tableCardC) return
+                     if (tableCardC.calculusValue >= cardValue) return
+                     if (tableCardA.calculusValue + tableCardB.calculusValue + tableCardC.calculusValue + tableCardD.calculusValue > cardValue) return
+                     if (tableCardA.calculusValue + tableCardB.calculusValue + tableCardC.calculusValue + tableCardD.calculusValue === cardValue) {
+                        if (combi && combi.includes(tableCardA) || combi.includes(tableCardB) || combi.includes(tableCardC) || combi.includes(tableCardD)) return
+                        if (combi && combi.includes(card)) { combi.push(tableCardA, tableCardB, tableCardC, tableCardD) } else { combi.push(card, tableCardA, tableCardB, tableCardC, tableCardD) }
+                     }
+                  })
+               })
+            })
+
+            // console.log("🚀 ~ file: GameWindow.js ~ line 156 ~ arr.map ~ combi", combi)
+            if (!combi || !combi.length) return
+            combinations.push(combi)
+         })
+      })
+   }
+
    const compMove = async () => {
       isPlayersMove(false)
       console.log("🚀 ~ file: GameWindow.js ~ line 88 ~ compMove ~ store.compInHandCards", store.compInHandCards.map(card => card.calculusValue))
-      
-      const combinations = []
-      
-      const cardsValuesForCalculating = Object.values(store.cardsOnTable).map(card => card.calculusValue)
-      cardsValuesInCalculation = [...cardsValuesForCalculating]
-      
-      store.compInHandCards.map(card => {
-         let combi = []
 
-         //  same card
-         store.cardsOnTable.map((tableCard, i) => card.calculusValue === tableCard.calculusValue && combi.push(card, tableCard))
-         // pair
-         store.cardsOnTable.map((tableCard, i) => {
-            if (card.calculusValue - tableCard.calculusValue > 0) {
-               const pairedCard = store.cardsOnTable.find((pairCard,i) => pairCard.calculusValue === card.calculusValue - tableCard.calculusValue)
-               if (!pairedCard) return
-               console.log("🚀 ~ file: GameWindow.js ~ line 102 ~ store.cardsOnTable.map ~ pairedCard", pairedCard)
-               console.log("🚀 ~ file: GameWindow.js ~ line 104 ~ store.cardsOnTable.map ~ combinations[i]", combinations[i])
-               // if (combinations[i] && combinations[i].map(cardCombinations => cardCombinations.id === pairedCard.id || cardCombinations.id === card.id)) return
-               // combi.some(card => card.id === pairedCard.id || card.id === tableCard.id)
-               // console.log("🚀 ~ file: GameWindow.js ~ line 104 ~ store.cardsOnTable.map ~ isItThere", isItThere)
-               if(combi && combi.includes(tableCard) || combi.includes(pairedCard)) return
-               if(combi && !combi.includes(card)) combi.push(card)
-               pairedCard && combi.push(tableCard, pairedCard)
-            }
-         })
+      checkComputerCalculus(store.compInHandCards)
+      const isAceThere = store.compInHandCards.some(card => card.calculusValue === 1)
+      isAceThere && checkComputerCalculus(store.compInHandCards, true)
 
-         console.log("🚀 ~ file: GameWindow.js ~ line 95 ~ compMove ~ combi", combi)
-         combinations.push(combi)
-      })
-      
-      store.compInHandCards.map(card => {
-         let combi = []
-
-         // a + b + c
-         store.cardsOnTable.map((tableCard, i) => {
-            console.log("🚀 ~ file: GameWindow.js ~ line 124 ~ store.cardsOnTable.map ~ tableCard[i+1]", store.cardsOnTable[i+1])
-            if (!store.cardsOnTable[i+1]) return
-            console.log('is it passing ???????')
-            let thirdValue = store.cardsOnTable[i+1].calculusValue
-            let thirdCard = store.cardsOnTable[i+1].calculusValue
-            if (card.calculusValue - tableCard.calculusValue - thirdValue > 0) {
-               let pairedCard = store.cardsOnTable.find((pairCard,i) => pairCard.calculusValue === card.calculusValue - tableCard.calculusValue - thirdValue)
-               if (!store.cardsOnTable[i+2]) { return } else { thirdCard = store.cardsOnTable[i+2]; thirdValue = store.cardsOnTable[i+2].calculusValue  } 
-               if (!pairedCard && thirdValue) {
-                  pairedCard = store.cardsOnTable.find((pairCard,i) => pairCard.calculusValue === card.calculusValue - tableCard.calculusValue - thirdValue)
-                  console.log("🚀 ~ IT IS HEREEE!!! THREE #@%#@%#@%d", pairedCard)
-               }
-               if (!pairedCard) return
-               console.log("🚀 ~ THREE #@%#@%#@%d", pairedCard)
-               // console.log("🚀 ~ file: GameWindow.js ~ line 104 ~ store.cardsOnTable.map ~ combinations[i]", combinations[i])
-               
-               if(combi && combi.includes(tableCard) || combi.includes(store.cardsOnTable[i+1]) || combi.includes(store.cardsOnTable[i+2]) || combi.includes(pairedCard)) return
-               if(combi && !combi.includes(card)) combi.push(card)
-               pairedCard && combi.push(tableCard, pairedCard, thirdCard)
-            }
-         })
-
-         
-         //  same card
-         store.cardsOnTable.map((tableCard, i) => {
-            if (card.calculusValue === tableCard.calculusValue) combi.push(tableCard)
-            if(combi && !combi.includes(card)) combi.push(card)
-         })
-
-         console.log("🚀 ~ file: GameWindow.js ~ line 95 ~ compMove ~ combi", combi)
-         combinations.push(combi)
-      })
-      
 
       console.log("🚀 ~ file: GameWindow.js ~ line 89 ~ compMove ~ combinations", combinations)
-      // console.log("🚀 ~ file: GameWindow.js ~ line 96 ~ compMove ~ cardsValuesInCalculation", cardsValuesInCalculation)
       await delay(3000)
       console.log('after first await')
       await delay(2000)
       console.log('after second await')
-      
+
       isPlayersMove(true)
    }
 
    const playerCardClickHandler = (id, playerCard) => {
-      
+
       if (!playersMove) return
 
-      if(store.cardsInCalculation.length === 0) {
+      if (store.cardsInCalculation.length === 0) {
          console.log('THROW PLAYER CARD')
 
          store.cardsOnTable.push(store.playerInHandCards.find(playerCard => playerCard.id === id))
@@ -177,7 +179,7 @@ const GameWindow = () => {
          compMove()
          return
       }
-      
+
       const cardsValuesForCalculating = Object.values(store.cardsInCalculation).map(card => card.calculusValue)
       cardsValuesInCalculation = [...cardsValuesForCalculating]
       checkCalculus(playerCard, cardsValuesForCalculating)
@@ -186,7 +188,7 @@ const GameWindow = () => {
       // if player marked cards on table -> do the calculus check
       if (cardsValuesInCalculation.length === 0) {
          console.log("🚀 ~ file: GameWindow.js ~ line 85 ~ playerCardClickHandler ~ store.cardsOnTable", store.cardsOnTable)
-      
+
          store.cardsOnTable.map((tableCard, tableIndex) => {
 
             console.log("🚀 ~ file: GameWindow.js ~ line 101 ~ store.cardsInCalculation.map ~ store.cardsInCalculation", store.cardsInCalculation)
@@ -198,7 +200,7 @@ const GameWindow = () => {
                   store.playerCollectedCards.push(tableCard)
                   store.cardsOnTable = store.cardsOnTable.filter(tableCard => tableCard !== calcCard)
                   console.log("🚀 ~ file: GameWindow.js ~ line 93333 ~ store.cardsInCalculation.map ~ store.cardsOnTable", store.cardsOnTable)
-                  
+
                   const playerCard = store.playerInHandCards.find(item => item.id === id)
                   store.playerCollectedCards.push(playerCard)
                   store.playerInHandCards = store.playerInHandCards.filter(card => card !== playerCard)
@@ -208,7 +210,7 @@ const GameWindow = () => {
                   console.log('it is pushed and removed')
 
                }
-               
+
                // const newArray = store.cardsOnTable.filter(tableCard.id !== calcCard.id)
                // store.cardsOnTable = newArray
                // console.log("🚀 ~ file: GameWindow.js ~ line 103 ~ store.cardsInCalculation.map ~ store.cardsOnTable", store.cardsOnTable)
@@ -217,15 +219,15 @@ const GameWindow = () => {
 
          console.log('clear cardsInCalc array')
          store.cardsInCalculation.splice(0, store.cardsInCalculation.length)
-      
+
       } else {
          console.log('player calc is WRONG!')
          return
       }
 
-   
+
       compMove()
-      
+
    }
 
 
@@ -258,7 +260,7 @@ const GameWindow = () => {
                <div className={'opponentFieldBuffer'}></div>
             </div>
 
-            <Playground playersMove={playersMove}/>
+            <Playground playersMove={playersMove} />
 
             {/* PLAYER AREA */}
             <div className={'playerField'}>
@@ -286,3 +288,66 @@ const GameWindow = () => {
 }
 
 export default GameWindow;
+
+
+
+
+      // store.compInHandCards.map(card => {
+      //    let combi = []
+
+      //    //  same card
+      //    store.cardsOnTable.map((tableCard, i) => card.calculusValue === tableCard.calculusValue && combi.push(card, tableCard))
+      //    // pair
+      //    store.cardsOnTable.map((tableCard, i) => {
+      //       if (card.calculusValue - tableCard.calculusValue > 0) {
+      //          const pairedCard = store.cardsOnTable.find((pairCard,i) => pairCard.calculusValue === card.calculusValue - tableCard.calculusValue)
+      //          if (!pairedCard) return
+      //          // console.log("🚀 ~ file: GameWindow.js ~ line 102 ~ store.cardsOnTable.map ~ pairedCard", pairedCard)
+      //          // console.log("🚀 ~ file: GameWindow.js ~ line 104 ~ store.cardsOnTable.map ~ combinations[i]", combinations[i])
+      //          // if (combinations[i] && combinations[i].map(cardCombinations => cardCombinations.id === pairedCard.id || cardCombinations.id === card.id)) return
+      //          // combi.some(card => card.id === pairedCard.id || card.id === tableCard.id)
+      //          // console.log("🚀 ~ file: GameWindow.js ~ line 104 ~ store.cardsOnTable.map ~ isItThere", isItThere)
+      //          if(combi && combi.includes(tableCard) || combi.includes(pairedCard)) return
+      //          if(combi && !combi.includes(card)) combi.push(card)
+      //          pairedCard && combi.push(tableCard, pairedCard)
+      //       }
+      //    })
+
+      //    // console.log("🚀 ~ file: GameWindow.js ~ line 95 ~ compMove ~ combi", combi)
+      //    combinations.push(combi)
+      // })
+      // const areEqual = (array1, array2) => {
+      //    console.log('areEqual called!!!!!')
+      //    console.log("🚀 ~ file: GameWindow.js ~ line 118 ~ areEqual ~ array1",typeof( array1.sort().join(',')))
+      //    console.log("🚀 ~ file: GameWindow.js ~ line 118 ~ areEqual ~ array2", array2.sort().join(','))
+      //    if (array1?.length === array2?.length) {
+      //       console.log('length eqal!!@!!!')
+      //       if(array1.sort().join(',') !== array2.sort().join(',')) {return true}
+      //       else { return false }
+      //       // return array1.every((element) => {
+      //       //    console.log("🚀 ~ file: GameWindow.js ~ line 120 ~ returnarray1.every ~ element", element)
+      //       //    if (array2.includes(element)) {
+      //       //       // console.log('THEY ARE EQUALL !!! $^*$#**$#%*$#%**$%%*$#$*#*%*#*#$')
+      //       //       return true;
+      //       //    }
+
+      //       //    return false;
+      //       // });
+      //    }
+
+      //    return false;
+      // }
+
+      // function areEqual(array1, array2) {
+      //    if (array1.length === array2.length) {
+      //       return array1.every(element => {
+      //          if (array2.includes(element)) {
+      //             return true;
+      //          }
+
+      //          return false;
+      //       });
+      //    }
+
+      //    return false;
+      // }
